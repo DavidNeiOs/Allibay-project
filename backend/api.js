89aca14176eb3e0c52ebe243c92c6d5209fb684e
smamehdi <sma.mehdi@gmail.com>
2018-08-08 12:47:58 -0400

    finished most enpoints, only getItemsBought left

--- backend/api.js
+++ backend/api.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const replace = require('replace');
 const bodyParser = require('body-parser');
 const sha256 = require('sha256');
 const fs = require('fs');
@@ -145,18 +146,121 @@ app.post('/sellItem', (req,res) => {
     }
 })
 
+
+app.post('/purchaseItem', (req,res) => {
+    
+    let reqBody = JSON.parse(req.body.toString());
+    let cartData = reqBody.cart;
+    var buyerId = parseInt(reqBody.userId);
+    //find username with userId in userData.json
+    let buyerData = fs.readFileSync("./backend/userData.json").toString().split('\n').filter(x => x !== "").map(x => JSON.parse(x)).filter(x => x.userId == buyerId);
+    //verify if user has a session in serverState
+    let sess = serverState.sessions.filter(usr => usr.username === buyerData[0].username);
+    if (sess.length > 0) {
+        //if yes, 
+        //get itemsIds from cartData and append to itemsPurchList
+        let itemsPurchLst = cartData.map(x => x.itemId)
+        //find if items are available by verifying itemsData.json
+        let allItems = fs.readFileSync("./backend/itemsData.json").toString().split('\n').filter(x => x !== "").map(x => JSON.parse(x));
+        let allItemsBefore = fs.readFileSync("./backend/itemsData.json").toString().split('\n').filter(x => x !== "").map(x => JSON.parse(x));
+
+        let beforePurchItems = [];
+        for (i=0;i<itemsPurchLst.length;i++) {
+            let itemFound = allItemsBefore.find(x => x.itemId == itemsPurchLst[i])
+            beforePurchItems.push(itemFound);
+        }
+        let availItems = [];
+        for (i=0;i<itemsPurchLst.length;i++) {
+            let itemFound = allItems.find(x => x.itemId == itemsPurchLst[i])
+            availItems.push(itemFound);
+        }
+        //if not avail, res.send error items out of stock
+        if (availItems.length < 1) {
+            res.send(JSON.stringify({success:false,reason:"items were not found or are out of stock"}))
+        }
+        let boughtItems = []
+        
+        for (i=0;i<cartData.length;i++) {
+            var qty = parseInt(cartData[i].qtyPurchased)
+            availItems.forEach(x => {
+                if (x.itemId==cartData[i].itemId) {
+                    var itmBought = Object.assign({}, x)
+                    x.stock = (parseInt(x.stock)-qty).toString()
+                    itmBought.stock = qty.toString()
+                    boughtItems.push(itmBought)
+                }
+            })
+        }
+        let priceTotal = 0;
+        for (i=0;i<boughtItems.length;i++) {
+            priceTotal = priceTotal + parseInt(boughtItems[i].price)
+        }
+        console.log(boughtItems,availItems,beforePurchItems)
+        //if available, use replace module to change the necessary data in itemsData.json
+        //for loop array of objects containing regex & replax , to change itemsData.json
+        for (i=0;i<beforePurchItems.length;i++) {
+            replace({
+                regex: JSON.stringify(beforePurchItems[i]),
+                replacement: JSON.stringify(availItems[i]),
+                paths: ["./backend/itemsData.json"],
+                recursive: true,
+                silent: true,
+                });
+        }
+        //create transaction (itemId, sellerId, buyerId, txnTime, price, session, generate buyerId)
+        let newTxn = {
+            transactionID : Math.floor(Math.random()*100000),
+            date : new Date() / 1 ,
+            price : priceTotal,
+            txnStatus : 0,
+            sessionID : sess[0].token,
+            items : boughtItems,
+            buyerId : buyerId,
+        }
+        //append txn to txnData.json
+        fs.appendFileSync('./backend/txnData.json', JSON.stringify(newTxn) + '\n')
+        res.send(JSON.stringify({success:true,transaction:newTxn}))
+    } else {
+    //if not, send res.send error session timed out
+        res.send(JSON.stringify({success:false,reason:"session timed out"}))
+    }
+    
+})
+
+app.get('/itemsBought', (req,res) => {
+
+})
 app.get('/findItemById', (req,res) => {
     let search = req.query.itemID;
     let itemsList = fs.readFileSync('./backend/itemsData.json').toString().split('\n').filter(x => x !== "").map(x => JSON.parse(x));
     let filteredItems = itemsList.filter(itemObj => itemObj.itemId == search)
     if (filteredItems.length>0) {
-        let respFiltItem = JSON.stringify({success:true,itemFound:filteredItems[0]})
-        res.send(respFiltItem)
+        res.send(JSON.stringify({success:true,itemFound:filteredItems[0]}))
     } else {
-        let respFailFiltItem = JSON.stringify({success:false,reason:"invalid item id"})
-        res.send(respFailFiltItem)
+        res.send(JSON.stringify({success:false,reason:"invalid item id"}))
     }
 })
+
+app.get('/getAllItems', (req,res) => {
+    try {
+        let itemsList = fs.readFileSync('./backend/itemsData.json').toString().split('\n').filter(x => x !== "").map(x => JSON.parse(x));
+        res.send(JSON.stringify({success:true,items:itemsList}))
+    } catch(err) {
+        res.send(JSON.stringify({success:false,items:"error"}))
+    }
+})
+
+app.get('/soldItemsHistory', (req,res) => {
+    let search = req.query.userID;
+    let itemsList = fs.readFileSync('./backend/itemsData.json').toString().split('\n').filter(x => x !== "").map(x => JSON.parse(x));
+    let filteredItems = itemsList.filter(itemObj => itemObj.sellerId == search)
+    if (filteredItems.length>0) {
+        res.send(JSON.stringify({success:true,itemFound:filteredItems}))
+    } else {
+        res.send(JSON.stringify({success:false,reason:"invalid seller id"}))
+    }
+})
+
 app.get('/init', (req, res) => {
     let sessionID = getSessionIdFromCookie(req);
     //verify if is a user has already a valid session
@@ -168,7 +272,17 @@ app.get('/init', (req, res) => {
     }
 });
 
-
+app.post('/logout', (req,res) => {
+    let usrLogOut = JSON.parse(req.body.toString());
+    let sess = serverState.sessions.filter(user => user.username !== usrLogOut.username);
+    if (sess.length < serverState.sessions.length) {
+        serverState.sessions = sess
+        console.log(serverState)
+        res.send(JSON.stringify({success:true, description:"user logged out"}))
+    } else {
+        res.send(JSON.stringify({success:false, description:"user logged NOT out"}))
+    }
+})
 
 function getSessionIdFromCookie(req) {
     let sessionID = req.headers.cookie != undefined ? req.headers.cookie.split("=")[1] : "";
